aa32276d9e1b8b47b9bbfebf5210b67320c66faf
Emanoel <emanoelbamorin@gmail.com>
2021-04-16 11:31:48 -0300

    chore: ad jest to lint staged

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,10 @@
-
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest',
+    '.+\\.ts$': 'ts-jest'
   }
 }
